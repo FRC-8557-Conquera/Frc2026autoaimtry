d6a58a3dc16bae983a312f81f0812533249611e3
saljhyigit <salihyigitkaymaz@icloud.com>
2026-01-20 09:52:14 +0300

    SignalLogger Fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
