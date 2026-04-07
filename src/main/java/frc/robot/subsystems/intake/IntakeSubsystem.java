df6f799cf412b03e8a043e12e91965a448313c69
anaktonik <73789337+anaktonik@users.noreply.github.com>
2026-04-07 04:13:46 +0300

    migrate to yams

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,85 +1,66 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Seconds;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import yams.gearing.MechanismGearing;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class IntakeSubsystem extends SubsystemBase {
 
     private final TalonFX deployMotorLeft = new TalonFX(Constants.Intake.intakesol);
     private final TalonFX deployMotorRight = new TalonFX(Constants.Intake.intakesag);
-    private final TalonFX rollerMotor = new TalonFX(Constants.Intake.rollerMotorID);
-    
-    private final MotionMagicVoltage positionRequest = new MotionMagicVoltage(0).withEnableFOC(false);
-    private final DutyCycleOut dutyCycleRequest = new DutyCycleOut(0).withEnableFOC(false);
+    private final TalonFX rollerMotorRaw = new TalonFX(Constants.Intake.rollerMotorID);
 
-    // Başlangıçta hedefimiz 0 tur (Yani tam kapalı pozisyon)
-    private double targetPositionRotations = 0.0;
+    @SuppressWarnings("unchecked")
+    private final SmartMotorControllerConfig deployConfig = new SmartMotorControllerConfig(this)
+            .withGearing(new MechanismGearing(1))
+            .withIdleMode(MotorMode.BRAKE)
+            .withTelemetry("IntakeDeployMotor", TelemetryVerbosity.HIGH)
+            .withSupplyCurrentLimit(Amps.of(40))
+            .withMotorInverted(false)
+            .withOpenLoopRampRate(Seconds.of(0))
+            .withControlMode(ControlMode.OPEN_LOOP)
+            .withFollowers(Pair.of(deployMotorRight, true));
+
+    private final SmartMotorController deploySMC = new TalonFXWrapper(deployMotorLeft, DCMotor.getKrakenX60(1), deployConfig);
+
+    private final SmartMotorControllerConfig rollerConfig = new SmartMotorControllerConfig(this)
+            .withGearing(new MechanismGearing(1))
+            .withIdleMode(MotorMode.COAST)
+            .withTelemetry("IntakeRollerMotor", TelemetryVerbosity.HIGH)
+            .withSupplyCurrentLimit(Amps.of(30))
+            .withMotorInverted(false)
+            .withOpenLoopRampRate(Seconds.of(0))
+            .withControlMode(ControlMode.OPEN_LOOP);
+
+    private final SmartMotorController rollerSMC = new TalonFXWrapper(rollerMotorRaw, DCMotor.getKrakenX60(1), rollerConfig);
 
     public IntakeSubsystem() {
-        configureMotors();
     }
 
-    private void configureMotors() {
-        TalonFXConfiguration deployConfig = new TalonFXConfiguration();
-        deployConfig.CurrentLimits.SupplyCurrentLimit = 40;
-        deployConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    public void setRollerPower(double power) {
+        rollerSMC.setDutyCycle(power);
+    }
 
-        deployMotorLeft.getConfigurator().apply(deployConfig);
-        deployMotorRight.getConfigurator().apply(deployConfig);
-
-        // Mükemmel frenleme için Brake modu şart
-        deployMotorLeft.setNeutralMode(NeutralModeValue.Brake);
-        deployMotorRight.setNeutralMode(NeutralModeValue.Brake);
-
-        deployMotorRight.setControl(new Follower(deployMotorLeft.getDeviceID(),MotorAlignmentValue.Opposed));
-
-        TalonFXConfiguration rollerConfig = new TalonFXConfiguration();
-        rollerConfig.CurrentLimits.SupplyCurrentLimit = 30;
-        rollerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-        
-        rollerMotor.getConfigurator().apply(rollerConfig);
-        rollerMotor.setNeutralMode(NeutralModeValue.Coast); 
+    public void set(double duty) {
+        deploySMC.setDutyCycle(duty);
     }
 
     @Override
     public void periodic() {
-        /* double currentRotations = deployMotorLeft.getPosition().getValueAsDouble();
-
-        // Hedefi güvenlik sınırları içine al (0.0 ile MAX arasında tut, mekaniği kırma)
-        double clampedRotations = MathUtil.clamp(targetPositionRotations, 0.0, Constants.Intake.MAX_EXTENSION_ROTATIONS);
-        
-        // Motion Magic ile Kraken'i hedefe yolla. 
-        // Sen hedefi 0 yaptığında motor 0 noktasına zımk diye gidip Brake ile tutunacak.
-        deployMotorLeft.setControl(positionRequest.withPosition(clampedRotations)); */
-
-        //SmartDashboard.putNumber("Intake/Mevcut_Tur", currentRotations);
-        //SmartDashboard.putNumber("Intake/Hedef_Tur", targetPositionRotations);
+        deploySMC.updateTelemetry();
+        rollerSMC.updateTelemetry();
     }
-
-    // --- KOMUTLAR İÇİN KULLANILACAK METOTLAR ---
-    
-    /*public void setDeployTargetRotations(double rotations) {
-        this.targetPositionRotations = rotations;
-    }*/
-
-    public void setRollerPower(double power) {
-        rollerMotor.setControl(dutyCycleRequest.withOutput(power));
-    }
-
-    public void set(double duty) {
-        deployMotorLeft.setControl(dutyCycleRequest.withOutput(duty));
-    } 
-    
 }
-
-        
